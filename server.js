0c001a9080f118c57d4d689029926af489f559f7
rameshhashcodex <ramesh.hashcodex@gmail.com>
2026-05-14 11:22:44 +0530

    changes

--- server.js
+++ server.js
@@ -62,7 +62,8 @@ const allowedOrigins = [
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:3000",
-    "https://optiontrade.net"
+    "https://optiontrade.net",
+    "https://big-one.io"
 ];
 
 i18n.configure({
